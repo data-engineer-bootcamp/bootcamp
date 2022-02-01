466d289619f8fd3382f82acbd28fe273aeb299f4
Stanislav Gorchakov <stanislav.gorchakov@profitero.com>
2022-02-01 14:08:05 +0300

    add start branch

--- project_load_vk_data/persons.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS persons (
-    id INTEGER,
-    first_name VARCHAR(128),
-    last_name VARCHAR(128),
-    is_closed BOOLEAN,
-    sex INTEGER,
-    bdate DATE,
-    city VARCHAR(128)
-);
